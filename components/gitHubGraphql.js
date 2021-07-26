b21f157709d10303777344082a4c602982b01df4
endiesworld <endiesworld@gmail.com>
2021-07-26 09:09:04 +0100

    fix:Changed file Github... to gitgithub..

--- components/gitHubGraphql.js
+++ components/gitHubGraphql.js
@@ -1,5 +1,5 @@
 import Image from 'next/image' ;
-import DevPlaceImage from '../public/images/GithubRepositories.jpg';
+import DevPlaceImage from '../public/images/githubRepositories.jpg';
 
 
 export default function GitHubGraphQL(){
