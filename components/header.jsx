1fb3e8bbefd7e74a37b3b5704d56b998a22c4dc7
endiesworld <endiesworld@gmail.com>
2021-07-26 15:55:46 +0100

    fix: Change link of the about page to the right page

--- components/header.jsx
+++ components/header.jsx
@@ -10,7 +10,7 @@ function Header(){
         <Link href="/portfolio">
             <a className = 'flex-initial text-center hover:text-white hover:bg-pink-900 w-16 h-6 pt-1 rounded-md'> Portfolio </a>
         </Link>
-        <Link href="/contactMe">
+        <Link href="/about">
             <a className = 'flex-initial text-center hover:text-white hover:bg-pink-900 w-11 h-6 pt-1 rounded-md'> About  </a>
         </Link>
     </div >
